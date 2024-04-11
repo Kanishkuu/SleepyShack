d07e3b3e9da4007cfc7a793d1a3468fb11dad9f2
Kanishkuu <kanishkkabra01@gmail.com>
2024-04-12 01:33:55 +0530

    Front End

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+import { faPersonMilitaryRifle } from '@fortawesome/free-solid-svg-icons';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{ 
+         primary: '#003580' }
+    },
+  },
+  plugins: [],
+}
+
